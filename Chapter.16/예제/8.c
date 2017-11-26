54806080e517520f06c6df7fa04a03e1b5aa0620
alstn2468 <alstn2468_@naver.com>
2017-11-26 23:34:31 +0900

    2017-11-26

---
+++ Chapter.16/예제/8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	if (remove("sample.txt") == -1)
+	{
+		printf("sample.txt를 삭제할 수 없습니다.\n");
+	}
+	else
+	{
+		printf("sample.txt를 삭제하였습니다.\n");
+	}
+
+	return 0;
+}
